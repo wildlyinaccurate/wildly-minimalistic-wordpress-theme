8f87efff928219012c1d9c2a0d5e79afa023cd43
Joseph Wynn <joseph@wildlyinaccurate.com>
2013-05-30 18:35:53 +0100

    Run PhpStorm's 'Reformat Code' on lib

--- lib/theme-functions.php
+++ lib/theme-functions.php
@@ -2,7 +2,7 @@
 
 /****************************************
 Backend Functions
-*****************************************/
+ *****************************************/
 
 /**
  * Customize Contact Methods
@@ -14,11 +14,11 @@ Backend Functions
  * @param array $contactmethods
  * @return array
  */
-function mb_contactmethods( $contactmethods )
+function mb_contactmethods($contactmethods)
 {
-    unset( $contactmethods['aim'] );
-    unset( $contactmethods['yim'] );
-    unset( $contactmethods['jabber'] );
+    unset($contactmethods['aim']);
+    unset($contactmethods['yim']);
+    unset($contactmethods['jabber']);
 
     return $contactmethods;
 }
@@ -29,26 +29,30 @@ function mb_contactmethods( $contactmethods )
 function mb_widgets_init()
 {
     // Main Sidebar
-    register_sidebar(array(
-        'name'          => __( 'Main Sidebar', 'mb' ),
-        'id'            => 'main-sidebar',
-        'description'   => __( 'Widgets for Main Sidebar.', 'mb' ),
-        'before_widget' => '<section id="%1$s" class="widget %2$s">',
-        'after_widget'  => '</section>',
-        'before_title'  => '<h4 class="widget-title">',
-        'after_title'   => '</h4>'
-    ));
+    register_sidebar(
+        array(
+            'name' => __('Main Sidebar', 'mb'),
+            'id' => 'main-sidebar',
+            'description' => __('Widgets for Main Sidebar.', 'mb'),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget' => '</section>',
+            'before_title' => '<h4 class="widget-title">',
+            'after_title' => '</h4>'
+        )
+    );
 
     // Footer
-    register_sidebar(array(
-        'name'          => __( 'Footer', 'mb' ),
-        'id'            => 'footer-widgets',
-        'description'   => __( 'Widgets for Footer.', 'mb' ),
-        'before_widget' => '<section id="%1$s" class="widget %2$s">',
-        'after_widget'  => '</section>',
-        'before_title'  => '<h4 class="widget-title">',
-        'after_title'   => '</h4>'
-    ));
+    register_sidebar(
+        array(
+            'name' => __('Footer', 'mb'),
+            'id' => 'footer-widgets',
+            'description' => __('Widgets for Footer.', 'mb'),
+            'before_widget' => '<section id="%1$s" class="widget %2$s">',
+            'after_widget' => '</section>',
+            'before_title' => '<h4 class="widget-title">',
+            'after_title' => '</h4>'
+        )
+    );
 }
 
 /**
@@ -65,14 +69,15 @@ function mb_widgets_init()
  * @param string $url, request url
  * @return array request arguments
  */
-function mb_dont_update_theme( $r, $url )
+function mb_dont_update_theme($r, $url)
 {
-    if ( 0 !== strpos( $url, 'http://api.wordpress.org/themes/update-check' ) )
-        return $r; // Not a theme update request. Bail immediately.
-    $themes = unserialize( $r['body']['themes'] );
-    unset( $themes[ get_option( 'template' ) ] );
-    unset( $themes[ get_option( 'stylesheet' ) ] );
-    $r['body']['themes'] = serialize( $themes );
+    if (0 !== strpos($url, 'http://api.wordpress.org/themes/update-check')) {
+        return $r;
+    } // Not a theme update request. Bail immediately.
+    $themes = unserialize($r['body']['themes']);
+    unset($themes[get_option('template')]);
+    unset($themes[get_option('stylesheet')]);
+    $r['body']['themes'] = serialize($themes);
 
     return $r;
 }
@@ -98,9 +103,10 @@ function mb_remove_dashboard_widgets()
  */
 function mb_custom_menu_order($menu_ord)
 {
-    if (!$menu_ord) return true;
-    return array(
-        // 'index.php', // Dashboard
+    if (!$menu_ord) {
+        return true;
+    }
+    return array(// 'index.php', // Dashboard
         // 'separator1', // First separator
         // 'edit.php?post_type=page', // Pages
         // 'edit.php', // Posts
@@ -131,7 +137,7 @@ function mb_remove_menu_pages()
  */
 function mb_imagelink_setup()
 {
-    $image_set = get_option( 'image_default_link_type' );
+    $image_set = get_option('image_default_link_type');
     if ($image_set !== 'none') {
         update_option('image_default_link_type', 'none');
     }
@@ -149,7 +155,7 @@ function mb_unhide_kitchensink($args)
 
 /****************************************
 Frontend
-*****************************************/
+ *****************************************/
 
 /**
  * Enqueue scripts
@@ -157,17 +163,34 @@ Frontend
 function mb_scripts()
 {
     // CSS first
-    wp_register_style('mb_style', get_stylesheet_directory_uri().'/style.css', null, '1.0', 'all' );
-    wp_enqueue_style( 'mb_style' );
+    wp_register_style('mb_style', get_stylesheet_directory_uri() . '/style.css', null, '1.0', 'all');
+    wp_enqueue_style('mb_style');
     // JavaScript
-    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-        wp_enqueue_script( 'comment-reply' );
+    if (is_singular() && comments_open() && get_option('thread_comments')) {
+        wp_enqueue_script('comment-reply');
     }
-    if ( !is_admin() ) {
+    if (!is_admin()) {
         wp_enqueue_script('jquery');
-        wp_enqueue_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.6.2.min.js', false, NULL );
-        wp_enqueue_script('customplugins', get_template_directory_uri() . '/assets/js/plugins.min.js', array('jquery'), NULL, true );
-        wp_enqueue_script('customscripts', get_template_directory_uri() . '/assets/js/main.min.js', array('jquery'), NULL, true );
+        wp_enqueue_script(
+            'modernizr',
+            get_template_directory_uri() . '/assets/js/vendor/modernizr-2.6.2.min.js',
+            false,
+            null
+        );
+        wp_enqueue_script(
+            'customplugins',
+            get_template_directory_uri() . '/assets/js/plugins.min.js',
+            array('jquery'),
+            null,
+            true
+        );
+        wp_enqueue_script(
+            'customscripts',
+            get_template_directory_uri() . '/assets/js/main.min.js',
+            array('jquery'),
+            null,
+            true
+        );
     }
 }
 
@@ -188,10 +211,10 @@ function mb_remove_more_jump_link($link)
 {
     $offset = strpos($link, '#more-');
     if ($offset) {
-        $end = strpos($link, '"',$offset);
+        $end = strpos($link, '"', $offset);
     }
     if ($end) {
-        $link = substr_replace($link, '', $offset, $end-$offset);
+        $link = substr_replace($link, '', $offset, $end - $offset);
     }
 
     return $link;
