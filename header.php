4aa27b56454b34a67e3af12c13392979b3ac6740
Joseph Wynn <joseph@wildlyinaccurate.com>
2013-05-30 19:04:37 +0100

    Add Lato font

--- header.php
+++ header.php
@@ -7,27 +7,17 @@
     <meta charset="utf-8">
     <title><?php wp_title(''); ?></title>
     <meta name="viewport" content="width=device-width">
-    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/favicon.ico">
-    <link rel="apple-touch-icon"
-          href="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/apple-touch-icon.png">
+    <link href="http://fonts.googleapis.com/css?family=Lato:300,400" rel="stylesheet" type="text/css">
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<!--[if lt IE 8]>
-<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
-    your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to
-    improve your experience.</p>
-<![endif]-->
+    <div id="container">
+        <header>
+            <a id="logo" href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a>
+            <div class="description"><?php bloginfo('description'); ?></div>
+        </header>
 
-<div id="container">
-
-    <header>
-        <a id="logo" href="<?php echo home_url('/'); ?>"><?php bloginfo('name'); ?></a>
-
-        <div class="description"><?php bloginfo('description'); ?></div>
-    </header>
-
-    <nav>
-        <?php wp_nav_menu(array('theme_location' => 'primary')); ?>
-    </nav>
+        <nav>
+            <?php wp_nav_menu(array('theme_location' => 'primary')); ?>
+        </nav>
