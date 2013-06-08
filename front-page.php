07559a688662cd183b34cc6d59c621b18bc479e3
Joseph Wynn <joseph@wildlyinaccurate.com>
2013-06-08 19:59:01 +0100

    Link to posts from the front page

--- front-page.php
+++ front-page.php
@@ -6,7 +6,7 @@
 
         <article class="post" id="post-<?php the_ID(); ?>">
 
-            <h1><?php the_title(); ?></h1>
+            <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 
             <div class="entry">
 
