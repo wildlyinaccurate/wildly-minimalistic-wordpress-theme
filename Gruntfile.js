b041f8dc14f9367dc9ba685cfc92bb0a603ae41a
Joseph Wynn <joseph@wildlyinaccurate.com>
2013-06-15 19:16:06 +0100

    Remove htmlmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -170,27 +170,6 @@ module.exports = function (grunt) {
                 }
             }
         },
-        htmlmin: {
-            dist: {
-                options: {
-                    /*removeCommentsFromCDATA: true,
-                    // https://github.com/yeoman/grunt-usemin/issues/44
-                    //collapseWhitespace: true,
-                    collapseBooleanAttributes: true,
-                    removeAttributeQuotes: true,
-                    removeRedundantAttributes: true,
-                    useShortDoctype: true,
-                    removeEmptyAttributes: true,
-                    removeOptionalTags: true*/
-                },
-                files: [{
-                    expand: true,
-                    cwd: '<%= config.app %>',
-                    src: '*.html',
-                    dest: '<%= config.dist %>'
-                }]
-            }
-        },
         // Put files not handled in other tasks here
         copy: {
             dist: {
