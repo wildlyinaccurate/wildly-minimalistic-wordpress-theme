f831bef90c47e80960da34eb0a4fff9093fd6c4b
Joseph Wynn <joseph@wildlyinaccurate.com>
2013-07-17 21:43:17 +0100

    Remove analytics from footer.
    This was mostly to prevent people using my analytics account (oops), but also
    because not everybody will want the analytics.

--- src/footer.php
+++ src/footer.php
@@ -8,17 +8,5 @@
         <script src="../src/assets/scripts/vendor/highlight.pack.js"></script>
         <script src="assets/scripts/highlight.js"></script>
         <!-- endbuild -->
-
-        <script>
-          var _gaq = _gaq || [];
-          _gaq.push(['_setAccount', 'UA-20744182-3']);
-          _gaq.push(['_trackPageview']);
-
-          (function() {
-            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-          })();
-        </script>
     </body>
 </html>
